8cd42686c69fc56a568e33eef7d55082929c80cb
Kinrany <kinrany@yandex.com>
2017-11-23 20:18:18 +0300

    loading questions from json

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
-let questions = [
-    {
-        id: 0,
-        text: "Как пройти в библиотеку?",
-        answers: [
-            {
-                id: 0,
-                text: "Направо", 
-            },
-            {
-                id: 1,
-                text: "Налево" 
-            }
-        ]
-    },
-    {
-        id: 1,
-        text: "Какой фреймворк лучше?",
-        answers: [ 
-            {
-                id: 0,
-                text: "React"
-            }, 
-            {
-                id: 1,
-                text: "Angular" 
-            },
-            {
-                id: 2,
-                text: "Vue"
-            }
-        ]
-    }
-];
+const data = {
+    questions: {},
+    answers: {}
+};
 
-let answers = {};
-questions.forEach(function (q) {
-    answers[q.id] = q.answers[0].id;
+let questionsRequest = $.getJSON("./questions.json");
+questionsRequest.done(function(jsonData) {
+    data.questions = jsonData.questions;
+
+    let answers = {};
+    data.questions.forEach(function (q) {
+        answers[q.id] = q.answers[0].id;
+    });
 });
 
 let app = new Vue({
     el: '#app',
-    data: {
-        questions: questions,
-        answers: answers
-    }
+    data: data
 });
